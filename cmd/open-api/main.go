f1cee71ce8debc20728206225ec488b8ef08ef24
Nurbek Abilev <abilev.nurbek@gmail.com>
2023-11-29 01:05:13 +0600

    upd main

--- cmd/open-api/main.go
+++ cmd/open-api/main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"net/http"
-
-	"github.com/swaggest/swgui/v5emb"
-)
-
 func main() {
-	http.Handle("/api1/docs/", v5emb.New(
-		"Petstore",
-		"https://petstore3.swagger.io/api/v3/openapi.json",
-		"/api1/docs/",
-	))
 
-	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		_, _ = writer.Write([]byte("Hello World!"))
-	})
-
-	println("docs at http://localhost:8080/api1/docs/")
-	_ = http.ListenAndServe("localhost:8080", http.DefaultServeMux)
 }
